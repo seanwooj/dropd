c40b8dad5c24f309c38dbf85b355b4a9042e5ef9
seanwooj <seanwoojinkim@gmail.com>
2015-03-31 19:07:39 -0700

    login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'signup' => 'users#new'
   root 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 end
