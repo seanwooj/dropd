b06502ff8fa99e1e4d66d7760a1994cf39cb8e37
seanwooj <seanwoojinkim@gmail.com>
2015-03-31 18:33:24 -0700

    signin works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #handle
+      flash[:success] = "Welcome to Dropd!"
+      redirect_to @user
     else
       render 'new'
     end
