4cfc92e76097ec8e19b372b15d1e07e3cc63661e
seanwooj <seanwoojinkim@gmail.com>
2015-03-31 18:23:15 -0700

    error messages work on user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
     :format => { :with =>  VALID_EMAIL_REGEX },
     :uniqueness => {:case_sensitive => false} )
 
-  validates(:password, :length => {:minimum => 6})
+  validates(:password, :length => {:minimum => 6}, :presence => true)
 
 
   has_secure_password
